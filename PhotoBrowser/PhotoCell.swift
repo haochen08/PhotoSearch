d89b9afb3ebe2f50015e012c7b2f3df8c867d607
Hao Chen <chenhao@yahoo-inc.com>
2017-06-27 21:24:31 -0700

    Show photos on CollectionView

---
+++ PhotoBrowser/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  PhotoBrowser
+//
+//  Created by  Hao Chen on 6/27/17.
+//  Copyright © 2017 HaoChen. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    var assetId : String!
+    
+}
--- PhotoBrowser/ViewController.swift
+++ PhotoBrowser/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    let reusableId = "PhotoCell"
+    let imageManager = PHImageManager()
+    let thumbnailSize = CGSize(width: 120, height: 120)
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var fetchResults : PHFetchResult<PHAsset>!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let options = PHFetchOptions()
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        fetchResults = PHAsset.fetchAssets(with: options)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusableId, for: indexPath) as! PhotoCell
+        let asset = fetchResults.object(at: indexPath.row)
+        cell.assetId = asset.localIdentifier
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFit, options: nil) { (image, _) in
+            // In case the cell is recycled
+            if cell.assetId == asset.localIdentifier && (image != nil) {
+                cell.imageView.image = image
+            }
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchResults.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return thumbnailSize
+    }
 }
 
