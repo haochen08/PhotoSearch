185561f5faa121c78fe428d0e566c14c93369e4d
Hao Chen <chenhao@yahoo-inc.com>
2017-06-27 20:11:47 -0700

    Initial Commit

---
+++ PhotoBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoBrowser
+//
+//  Created by  Hao Chen on 6/27/17.
+//  Copyright © 2017 HaoChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
